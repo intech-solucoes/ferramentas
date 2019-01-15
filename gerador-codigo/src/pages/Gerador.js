38d386ec4a21aa78bd3f8c3286c857fa36ac91cb
Rony Moura <ronymmoura@gmail.com>
2019-01-15 19:38:34 -0200

    WIP

--- gerador-codigo/src/pages/Gerador.js
+++ gerador-codigo/src/pages/Gerador.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import { Box } from "@intechprev/componentes-web";
+import { Box, Row, Col, Button } from "@intechprev/componentes-web";
+import Checkbox from "rc-checkbox";
+import 'rc-checkbox/assets/index.css';
 
 import { MasterPage } from "./";
 
 export default class Gerador extends Component {
+
+    state = {
+        gerarEntidade: true,
+        gerarDAO: true,
+        gerarProxy: true
+    }
 
     constructor(props) {
         super(props);
@@ -15,8 +23,65 @@ export default class Gerador extends Component {
         return (
             <MasterPage {...this.props} ref={this.page}>
                 
+                <Box titulo={"Gerador"}>
+                    
+                    <Row>
+                        <Col>
+                            <Row formGroup>
+                        
+                                <Col tamanho={"lg-12"} className={"col-form-label"}>
+                                    <label htmlFor={this.props.nome}>
+                                        Sistema
+                                    </label>
+                                </Col>
+
+                                <Col>
+                                    <select className={"form-control"}>
+                                        <option>Teste</option>
+                                    </select>
+                                </Col>
+
+                            </Row>
+                        </Col>
+
+                        <Col>
+                            <Row formGroup>
+                        
+                                <Col tamanho={"lg-12"} className={"col-form-label"}>
+                                    <label htmlFor={this.props.nome}>
+                                        Conexão
+                                    </label>
+                                </Col>
+
+                                <Col>
+                                    <select className={"form-control"}>
+                                        <option>Teste</option>
+                                    </select>
+                                </Col>
+
+                            </Row>
+                        </Col>
+                    </Row>
+
+                </Box>
+
                 <Box>
-                    Gerador
+
+                    <Row>
+                        <Col tamanho={"2"} className={"mt-2"}>
+                            <Checkbox />&nbsp; Entidade
+                        </Col>
+                        <Col tamanho={"2"} className={"mt-2"}>
+                            <Checkbox checked={this.state.gerarDAO} />&nbsp; DAO
+                        </Col>
+                        <Col tamanho={"2"} className={"mt-2"}>
+                            <Checkbox checked={this.state.gerarProxy} />&nbsp; Proxy
+                        </Col>
+                        <Col tamanho={"2"} className={"offset-md-4"}>
+                            <Button titulo={"Gerar"} tipo={"primary"} block />
+                        </Col>
+                    </Row>
+
                 </Box>
 
             </MasterPage>
