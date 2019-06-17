94ef81d7ca6a694a00003956a587fc890d53a939
Rony <ronymmoura@gmail.com>
2019-06-17 14:12:17 -0300

    WIP

---
+++ Intech.Ferramentas.GeradorCodigo/Intech.Ferramentas.GeradorCodigo/Controles/ControleProjetos.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Intech.Ferramentas.GeradorCodigo.Controles
+{
+    partial class ControleProjetos
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxProjetos = new System.Windows.Forms.ListBox();
+            this.ButtonNovo = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxRepositorio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxTipoProjeto = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ListBoxProjetos
+            // 
+            this.ListBoxProjetos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ListBoxProjetos.FormattingEnabled = true;
+            this.ListBoxProjetos.Location = new System.Drawing.Point(3, 32);
+            this.ListBoxProjetos.Name = "ListBoxProjetos";
+            this.ListBoxProjetos.Size = new System.Drawing.Size(173, 355);
+            this.ListBoxProjetos.TabIndex = 0;
+            // 
+            // ButtonNovo
+            // 
+            this.ButtonNovo.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ButtonNovo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ButtonNovo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ButtonNovo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonNovo.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.ButtonNovo.Location = new System.Drawing.Point(3, 3);
+            this.ButtonNovo.Name = "ButtonNovo";
+            this.ButtonNovo.Size = new System.Drawing.Size(173, 23);
+            this.ButtonNovo.TabIndex = 9;
+            this.ButtonNovo.Text = "Nova";
+            this.ButtonNovo.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TextBoxTipoProjeto);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.TextBoxRepositorio);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(183, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(319, 383);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configurações";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tipo de Projeto";
+            // 
+            // TextBoxRepositorio
+            // 
+            this.TextBoxRepositorio.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxRepositorio.Location = new System.Drawing.Point(6, 45);
+            this.TextBoxRepositorio.Name = "TextBoxRepositorio";
+            this.TextBoxRepositorio.Size = new System.Drawing.Size(307, 20);
+            this.TextBoxRepositorio.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nome Repositório";
+            // 
+            // TextBoxTipoProjeto
+            // 
+            this.TextBoxTipoProjeto.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxTipoProjeto.Location = new System.Drawing.Point(6, 90);
+            this.TextBoxTipoProjeto.Name = "TextBoxTipoProjeto";
+            this.TextBoxTipoProjeto.Size = new System.Drawing.Size(307, 20);
+            this.TextBoxTipoProjeto.TabIndex = 9;
+            // 
+            // ControleProjetos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ButtonNovo);
+            this.Controls.Add(this.ListBoxProjetos);
+            this.Name = "ControleProjetos";
+            this.Size = new System.Drawing.Size(505, 394);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxProjetos;
+        private System.Windows.Forms.Button ButtonNovo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TextBoxRepositorio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxTipoProjeto;
+    }
+}
---
+++ Intech.Ferramentas.GeradorCodigo/Intech.Ferramentas.GeradorCodigo/Controles/ControleProjetos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Intech.Ferramentas.GeradorCodigo.Controles
+{
+    public partial class ControleProjetos : UserControl
+    {
+        public ControleProjetos()
+        {
+            InitializeComponent();
+        }
+    }
+}
