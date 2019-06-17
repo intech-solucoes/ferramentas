a065a91bfcdad3feb59f561eefb241fbe41b9d80
Rony <ronymmoura@gmail.com>
2019-06-17 14:13:08 -0300

    WIP

--- Intech.Ferramentas.GeradorCodigo/Intech.Ferramentas.GeradorCodigo/Controles/ControleConexoes.Designer.cs
+++ Intech.Ferramentas.GeradorCodigo/Intech.Ferramentas.GeradorCodigo/Controles/ControleConexoes.Designer.cs
@@ -188,7 +188,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 72);
+            this.label2.Location = new System.Drawing.Point(9, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 2;
@@ -211,7 +211,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Location = new System.Drawing.Point(9, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 0;
